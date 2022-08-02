bee1c798bdf7fa9b87a7641dbc92e44091a0f73f
andyrcodes <92997141+andyrcodes@users.noreply.github.com>
2022-08-02 08:30:27 -0400

    Initial commit

---
+++ FizzBuzzMVC/Models/FBModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMVC.Models
+{
+    public class FBModel
+    {
+        public int FizzNum { get; set; }
+        public int BuzzNum { get; set; }
+        public int StartNum { get; set; }
+        public int EndNum { get; set; }
+        public List<string> Results { get; set; } = new List<string>();
+    }
+}
